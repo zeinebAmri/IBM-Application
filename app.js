2f9f6f52495ff93ef26e2f6b0d063acab11f0885
Infectiondg <linusg@hotmail.se>
2018-12-20 21:50:14 +0100

    Express and socket.io socket fix?

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var iotf = require("ibmiotf");
-var io = require("socket.io")(9090);
 var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require("socket.io")(http);
 var cfenv = require('cfenv');
 //------------------------------------------------------------------------------
 // create a new express server
-var app = express();
+
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -13,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+http.listen(appEnv.port, function() {
   console.log("server starting on " + appEnv.url);
 });
 
